53228f3cd069cd7eb5378a473c2194010ee8b7e6
Alan <alantsv@gmail.com>
2016-11-17 22:01:23 -0300

    SAVEPOINT 17-Nov-16

---
+++ arvore.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct arvore{
+	int dado;
+	struct arvore *sesq;
+	struct arvore *sdir;
+}arv;
+
+arv* arv_criarvazia(){
+	return NULL;
+}
+
+arv* arv_criar(int dado, arv *sdir, arv *sesq){
+	arv* tree= (arv*)malloc(sizeof(arv));
+	tree->dado =  dado;
+	tree->sesq = sesq;
+	tree->sdir = sdir;
+	return tree;
+}
+
+int arv_vazia(arv *tree){
+	return tree == NULL;
+}
+
+void arv_print (arv *tree){
+	printf("(");
+	if (!arv_vazia(tree)){
+		printf("%i", tree->dado);
+		arv_print(tree->sesq);
+		arv_print(tree->sdir);
+	}
+	printf(")");
+}
+
+int main (){
+	arv *tree;
+	tree = arv_criarvazia();
+	tree = arv_criar(1,
+			arv_criar(2,
+				arv_criarvazia(),
+				arv_criar(3, arv_criarvazia(), arv_criarvazia())
+			),
+			arv_criar(4,
+				arv_criar(5, arv_criarvazia(), arv_criarvazia()),
+				arv_criar(6, arv_criarvazia(), arv_criarvazia())
+			)
+		);
+	arv_print(tree);
+}
