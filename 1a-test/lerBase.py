a0705d70d0163f5f98d2ee34b0807cbecfc8d0ad
Alan Veloso <alantsv@gmail.com>
2017-01-09 12:37:23 -0300

    CHECKPOINT 09-Jan-17

--- 1a-test/lerBase.py
+++ 1a-test/lerBase.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 def openFile():
+	data = {}
 	ref_File = open("Dominios_GovBR_basico.csv", encoding="iso-8859-1")
 	ref_File.readline()
 	for line in ref_File:
 		info = line.split(";")
-		print("Local: %-20s Domínio: %s" % (info[4], info[0]))
+		print("Domínio: %s\nAcesso: %s" % (info[0], info[8]))
+		data.update({info[0]:info[8]})
+		print(data[info[0]])
 	ref_File.close()
 
 openFile()
---
+++ 1a-test/obst.py
@@ -0,0 +1,47 @@
+def suma (freq, i, j):
+	s = 0
+	for k in list(range(i,j+1)):
+		s += freq[k]
+	return s
+
+def optimalSearchTree(keys, freq, n):
+	cost = dict()
+	c = 0
+	for i in list(range(0,n)):
+		cost[(i,i)] = freq[i]
+	for L in list(range(0,n+1)):
+		for i in list(range(0,n-L+2)):
+			j = i + L-1
+			cost[(i,j)] = float("inf")
+		
+			for r in list(range(i,j+1)):
+				#c = cost[(i,r-1)] if r > i else 0 + cost[(r+1,j)] if r < j else 0 + sum(freq, i, j)
+				if r > i:
+					c = c + cost[(i,r-1)]
+				if r < j:
+					c = c + cost[(r+1,j)]
+				
+				c = c + suma(freq, i, j)
+
+				if (c < cost[(i,j)]):
+					cost[(i,j)] = c
+	return cost[(0, n-1)]
+
+
+def constructOBST (dict())
+	p = BinSearchTree()
+	if i == j:
+		p = None
+	else:
+		p = BinSearchTree()
+		p.key = 
+
+
+keys = [10,12,20]
+freq = [34,8,50]
+
+n = len(keys)
+
+num = optimalSearchTree(keys, freq, n)
+
+print ("Custo %s" % num)
